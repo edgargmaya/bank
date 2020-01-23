79d89435361bb7f81104a3c5c620ccec102f13b8
Edgar G <edgargmaya@gmail.com>
2020-01-22 22:48:29 -0600

    Adicion de archivo sql

---
+++ bank.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: Bank
+-- ------------------------------------------------------
+-- Server version	5.7.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cuenta`
+--
+
+DROP TABLE IF EXISTS `cuenta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cuenta` (
+  `numero_cuenta` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha_creacion` char(20) NOT NULL,
+  `monto` int(11) NOT NULL DEFAULT '0',
+  `tipo` varchar(10) NOT NULL DEFAULT 'b1',
+  PRIMARY KEY (`numero_cuenta`),
+  KEY `tipo` (`tipo`),
+  CONSTRAINT `cuenta_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipo_cuenta` (`tipo`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cuenta`
+--
+
+LOCK TABLES `cuenta` WRITE;
+/*!40000 ALTER TABLE `cuenta` DISABLE KEYS */;
+INSERT INTO `cuenta` VALUES (1,'22-01-2020',1000,'b1');
+/*!40000 ALTER TABLE `cuenta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo_cuenta`
+--
+
+DROP TABLE IF EXISTS `tipo_cuenta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipo_cuenta` (
+  `tipo` varchar(10) NOT NULL,
+  `monto_maximo` int(11) NOT NULL DEFAULT '0',
+  `descripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`tipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_cuenta`
+--
+
+LOCK TABLES `tipo_cuenta` WRITE;
+/*!40000 ALTER TABLE `tipo_cuenta` DISABLE KEYS */;
+INSERT INTO `tipo_cuenta` VALUES ('b1',5000,'Esta cuenta esta diseñada para alumnos y jovenes que crean una cuenta bancaria por primera vez');
+/*!40000 ALTER TABLE `tipo_cuenta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` char(20) NOT NULL,
+  `primer_apellido` char(20) NOT NULL,
+  `segundo_apellido` char(20) NOT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `numero_cuenta` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`),
+  KEY `numero_cuenta` (`numero_cuenta`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`numero_cuenta`) REFERENCES `cuenta` (`numero_cuenta`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-22 21:39:01
